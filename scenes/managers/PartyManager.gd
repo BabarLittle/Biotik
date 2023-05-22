1611f94acb3d547e866ed9ec8464557e746389e7
Ska <126164318+PalabraSka@users.noreply.github.com>
2023-05-22 11:01:42 +0200

    PartyManager: biomon editable teams

--- scenes/managers/PartyManager.gd
+++ scenes/managers/PartyManager.gd
@@ -3,20 +3,35 @@ extends Node2D
 var biodex_state = {}
 var boxes = ClassBoxes
 
+
+
 func _ready():
 	pass
-	
-func load_game(game_dictionary):
-	pass
-	
-func save_game():
-	var save_dictionary = {}
+
+
+func party_array_to_node(array_party) -> void:
+	for i in range(array_party.size()):
+		array_party[i].instance()
+
+
+func party_node_to_array() -> Array:
 	var party = []
 	
 	for child in get_children():
 		party.append(child.get_biomon_object())
 	
-	save_dictionary.party = party
+	return party
+
+
+func load_game(game_dictionary) -> void:
+	biodex_state = game_dictionary.biodex_state
+	party_array_to_node(game_dictionary.load_party)
+
+
+func save_game() -> Dictionary:
+	var save_dictionary = {}
+	
+	save_dictionary.party = party_node_to_array()
 	save_dictionary.boxes = boxes
 	save_dictionary.biodex_state = biodex_state
 	
